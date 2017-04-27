dcb091e152b674db7e78aab73d1165defe5a73c3
Rain <1101534564@qq.com>
2017-04-28 01:26:43 +0800

    #define PREVENT_COLD_EXTRUSION ////////////add/modify by wyl

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -239,7 +239,7 @@
 
 //this prevents dangerous Extruder moves, i.e. if the temperature is under the limit
 //can be software-disabled for whatever purposes by
-#define PREVENT_DANGEROUS_EXTRUDE
+#define PREVENT_COLD_EXTRUSION ////////////add/modify by wyl
 //if PREVENT_DANGEROUS_EXTRUDE is on, you can still disable (uncomment) very long bits of extrusion separately.
 #define PREVENT_LENGTHY_EXTRUDE
 
