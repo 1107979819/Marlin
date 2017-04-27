474f87af6d422d730fd44567a68674449b783104
Rain <1101534564@qq.com>
2017-04-28 01:29:16 +0800

    #define E0_AUTO_FAN_PIN   -1////////////add/modify by wyl #define E1_AUTO_FAN_PIN   -1////////////add/modify by wyl #define E2_AUTO_FAN_PIN   -1////////////add/modify by wyl

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -76,9 +76,9 @@
 // extruder temperature is above/below EXTRUDER_AUTO_FAN_TEMPERATURE.
 // Multiple extruders can be assigned to the same pin in which case
 // the fan will turn on when any selected extruder is above the threshold.
-#define EXTRUDER_0_AUTO_FAN_PIN   -1
-#define EXTRUDER_1_AUTO_FAN_PIN   -1
-#define EXTRUDER_2_AUTO_FAN_PIN   -1
+#define E0_AUTO_FAN_PIN   -1////////////add/modify by wyl
+#define E1_AUTO_FAN_PIN   -1////////////add/modify by wyl
+#define E2_AUTO_FAN_PIN   -1////////////add/modify by wyl
 #define EXTRUDER_AUTO_FAN_TEMPERATURE 50
 #define EXTRUDER_AUTO_FAN_SPEED   255  // == full speed
 
